4956481478e8b5939e051e2df6cd1d9d571473e0
Pedro Ramos <pedrodevop@gmail.com>
2020-12-18 17:09:04 +0100

    refactor preposition file loading

--- src/prepositions.rs
+++ src/prepositions.rs
@@ -4,16 +4,26 @@ use crate::types::PrepositionExercise;
 pub fn get_prepositions_exercises() -> Vec<PrepositionExercise> {
     let mut prepositions = Vec::new();
     if let Ok(lines) = read_file_lines("data/prepositions.txt") {
+        let mut preposition = "".to_owned();
+        let mut case = "".to_owned();
         for line in lines.iter() {
             if line == "" {
+                preposition = "".to_owned();
+                case = "".to_owned();
                 continue;
             }
 
-            let attr = line.split(";").collect::<Vec<&str>>();
-            let phrase = attr[0].to_owned();
-            let preposition = attr[1].to_owned();
+            let configuration_line = preposition == "" && case == "";
 
-            prepositions.push(PrepositionExercise { phrase, preposition });
+            if configuration_line {
+                let attr = line.split(";").collect::<Vec<&str>>();
+                preposition = attr[0].to_owned();
+                case = attr[1].to_owned();
+            } else {
+                let phrase = line.to_owned() + "(" + &case + ")";
+
+                prepositions.push(PrepositionExercise { phrase, preposition: preposition.to_owned() });
+            }
         }
     }
 
